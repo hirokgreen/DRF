f106f182ede0cbeb6f26f6c34baa9137736ce05c
hirok <biswashirok@gmail.com>
2017-02-17 17:59:55 +0600

    dockstring added

--- testapi/models.py
+++ testapi/models.py
@@ -1,11 +1,11 @@
+"""models classs for testapi app"""
 from __future__ import unicode_literals
-
 from django.db import models
 
 class Contact(models.Model):
-	name = models.CharField(max_length=20)
-	email = models.CharField(max_length=40)
-	address = models.CharField(max_length=400)
-
-	def __str__(self):
-		return self.name
+    """contact table with name,email and address, name will be the row title"""
+    name = models.CharField(max_length=20)
+    email = models.CharField(max_length=40)
+    address = models.CharField(max_length=400)
+    def __str__(self):
+        return self.name
